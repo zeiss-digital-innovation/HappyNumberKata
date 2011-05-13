838cd947b9618b9560f0a97e4a43ac7e866cfc23
Sebastian Schmeck <sebastian.schmeck@saxsys.de>
2011-05-13 23:01:04 +0200

    Eins ist gluecklich, Zwei nicht

--- src/test/java/de/saxsys/dojo/happynumber/HappyNumberTest.java
+++ src/test/java/de/saxsys/dojo/happynumber/HappyNumberTest.java
@@ -11,4 +11,18 @@ public class HappyNumberTest {
 	public void einsIstEineGluecklicheZahl() throws Exception {
 		assertThat(new NumberCheck().isHappy(1), is(true));
 	}
+
+	@Test
+	public void zweiIstKeineGluecklicheZahl() throws Exception {
+		// 2² = 4
+		// 4² = 16
+		// 1² + 6² = 1 + 36 = 37
+		// 3² + 7² = 9 + 49 = 58
+		// 5² + 8² = 25 + 64 = 89
+		// 8² + 9² = 64 + 81 = 145
+		// 1² + 4² + 5² = 1 + 16 + 25 = 42
+		// 4² + 2² = 16 + 4 = 20
+		// 2² = 4 ...
+		assertThat(new NumberCheck().isHappy(2), is(false));
+	}
 }
