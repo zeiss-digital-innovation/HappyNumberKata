fd6768fc65e5937672cb65c06087add1e2f255b2
Sebastian Schmeck <sebastian.schmeck@saxsys.de>
2011-05-15 15:35:29 +0200

    Refactoring: Implementierung aufgeraeumt

--- src/main/java/de/saxsys/dojo/happynumber/NumberCheck.java
+++ src/main/java/de/saxsys/dojo/happynumber/NumberCheck.java
@@ -10,32 +10,30 @@ public class NumberCheck {
 	 * @return <code>true</code>, falls es sich um eine glueckliche Zahl handelt
 	 */
 	public boolean isHappy(long number) {
+
 		long number2Check = number;
 		if (!startsWithOneAndFollowedByZeros(number2Check)) {
 			number2Check = getSumOfSquaresOfDigitsOf(number);
 		}
+
 		return startsWithOneAndFollowedByZeros(number2Check);
 	}
 
 	private long getSumOfSquaresOfDigitsOf(long number) {
 
-		long result = 0;
-
-		String numberString = String.valueOf(number);
+		long newNumber = 0;
+		final String numberString = String.valueOf(number);
 		for (int i = 0; i < numberString.length(); i++) {
-			long digit = Long.parseLong(String.valueOf(numberString.charAt(i)));
-			result += (digit * digit);
+			final long digit = Long.parseLong(String.valueOf( //
+					numberString.charAt(i)));
+			newNumber += digit * digit;
 		}
 
-		// Scanner sc = new Scanner(String.valueOf(number));
-		// while (sc.hasNext("[0-9]{1}")) {
-		// long digit = Long.parseLong(sc.next("[0-9]{1}"));
-		// result += (digit * digit);
-		// }
-		return result;
+		return newNumber;
 	}
 
 	private boolean startsWithOneAndFollowedByZeros(long number) {
+
 		return String.valueOf(number).matches("10*");
 	}
 }
